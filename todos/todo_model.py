5102221e6ba81587febf9d346f15adddf98f80e0
ttran <ms.tam.h.tran@gmail.com>
2024-04-01 13:44:58 -0700

    Added 2 fields, user_id to track owner of todo, test_tt to explore functionality

--- todos/todo_model.py
+++ todos/todo_model.py
@@ -18,6 +18,8 @@ class TodoModel(Model):
     checked = BooleanAttribute(null=False)
     createdAt = UTCDateTimeAttribute(null=False, default=datetime.now())
     updatedAt = UTCDateTimeAttribute(null=False)
+    user_id = UnicodeAttribute(null=False)
+    test_tt = UnicodeAttribute(null=True)
 
     def save(self, conditional_operator=None, **expected_values):
         self.updatedAt = datetime.now()
